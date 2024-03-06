743ac2c6e4a914ad94b9722871509e172717b977
EbenFlutt <ebenflutt@live.com>
2024-03-06 12:05:38 -0600

    Delete src directory

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders Not Wordle', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Not Wordle - YourLanguageHere/i)
-  expect(linkElement).toBeInTheDocument()
-})
